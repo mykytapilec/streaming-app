5ee2299008b2df065b78bbfc1bcda6a40d9e119e
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-02-16 15:20:37 +0100

    PORT was updated

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module"
 
 const start = async () => {
     try {
-        const PORT = process.env.PORT || 5000
+        const PORT = process.env.PORT || 5050
         const app = await NestFactory.create(AppModule)
 
         await app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))

00770492719ea834eb96f76d5891633ba53baec1
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-06-12 10:36:56 +0200

    Track types

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,13 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { Box, Button, Card, Grid2 } from "@mui/material"
 
 import MainLayout from "../../layouts/MainLayout"
 import { useRouter } from "next/router"
+import { ITrack } from "../../types/track"
 
 
 const Index = () => {
     const router = useRouter()
+    const [tracks, setTracks] = useState<ITrack[]>([
+        {
+            _id: '1',
+            name: 'Track 1',
+            artist: 'Artist 1',
+            text: 'Text 1',
+            listens: 100,
+            picture: 'picture 1',
+            audio: 'audio 1',
+            comments: []
+        },
+        {
+            _id: '2',
+            name: 'Track 2',
+            artist: 'Artist 2',
+            text: 'Text 2',
+            listens: 200,
+            picture: 'picture 2',
+            audio: 'audio 2',
+            comments: []
+        },
+        {
+            _id: '3',
+            name: 'Track 3',
+            artist: 'Artist 3',
+            text: 'Text 3',
+            listens: 300,
+            picture: 'picture 3',
+            audio: 'audio 3',
+            comments: []
+        }   
+    ])
 
     return (
         <MainLayout>
@@ -16,7 +49,10 @@ const Index = () => {
                     <Box p={2}>
                         <Grid2 container justifyContent="space-between">
                             <h1>Track 1</h1>
-                            <Button onClick={() => router.push('/tracks/create')}>Load</Button>
+                            <Button onClick={() => {
+                                console.log('clicked', router)
+                                router.push('/tracks/create')
+                            }}>Load</Button>
                         </Grid2>
                     </Box>   
                 </Card>

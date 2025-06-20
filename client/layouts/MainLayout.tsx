0299c51f60a58134f85b2c4d7f65780a08c35926
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-06-20 11:08:43 +0200

    Player component

--- client/layouts/MainLayout.tsx
+++ client/layouts/MainLayout.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react"
 
 import Navbar from "../components/Navbar"
+import Player from "../components/Player"
+import { Container } from "@mui/material"
 
 interface MainLayoutProps {
     children: ReactNode
@@ -8,10 +10,13 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     return (
-        <div style={{ marginTop: 100, background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.15) 0%, rgba(0, 0, 0, 0.15) 100%)', minHeight: '100vh' }}>
+        <>
             <Navbar />
-            {children}
-        </div>
+            <Container style={{margin: '90px 0'}}>
+                { children }
+            </Container>
+            <Player />
+        </>
     )
 }
 

29cd3bb4266c781473009e29115741406588de5e
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-07-11 15:15:36 +0200

    next step func

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,21 +1,45 @@
+import { useRouter } from "next/router";
 import FileUpload from "../../components/FileUpload";
 import StepWrapper from "../../components/StepWrapper";
+import { useInput } from "../../hooks/useInput";
 import MainLayout from "../../layouts/MainLayout";
 import { Button, Grid2, TextField } from "@mui/material";
 import { useState } from "react";
+import axios from "axios";
 
 const Create = () => {
     const [activeStep, setActiveStep] = useState(0)
     const [picture, setPicture] = useState<File | null>(null)
     const [audio, setAudio] = useState<File | null>(null)
+    const name = useInput('')
+    const artist = useInput('')
+    const text = useInput('')
+    const router = useRouter()
     
     const back = () => {
         setActiveStep(activeStep - 1)
     }
 
     const next = () => {
-        setActiveStep(activeStep + 1)
+        if (activeStep !== 2) {
+            setActiveStep(prev => prev + 1)
+        } else {
+            const formData = new FormData()
+            formData.append('name', name.value)
+            formData.append('text', text.value)
+            formData.append('artist', artist.value)
+            if (picture) {
+                formData.append('picture', picture)
+            }
+            if (audio) {
+                formData.append('audio', audio)
+            }
+            axios.post('http://localhost:5050/tracks', formData)
+                .then(() => router.push('/tracks'))
+                .catch(e => console.log(e))
+        }
     }
+
     return (
         <MainLayout>
             {activeStep === 0 && <StepWrapper activeStep={activeStep}>
